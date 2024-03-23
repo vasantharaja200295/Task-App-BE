42ee44b8a619ee0188f5e855ae738b4bd31105d7
Vasantharaja <vasantharaja200295@gmail.com>
2024-03-23 11:32:07 +0530

    initial commit

---
+++ db_service.py
@@ -0,0 +1,123 @@
+from pymongo import MongoClient, ReturnDocument
+from bson import ObjectId
+import json
+
+class MongoDB:
+    def __init__(self, db_name='test_database'):
+        self.client = MongoClient('mongodb://localhost:27017/')
+        self.db = self.client[db_name]
+
+    def create_document(self, collection_name, document):
+        collection = self.db[collection_name]
+        result = collection.insert_one(document)
+        return str(result.inserted_id)
+
+    def read_documents(self, collection_name, query={}):
+        collection = self.db[collection_name]
+        documents = collection.find(query)
+        return [self._serialize(doc) for doc in documents]
+
+    def read_document(self, collection_name, query):
+        collection = self.db[collection_name]
+        document = collection.find_one(query)
+        return self._serialize(document)
+
+    def update_documents(self, collection_name, query, new_values):
+        collection = self.db[collection_name]
+        result = collection.update_many(query, {"$set": new_values})
+        return result.modified_count
+
+    def update_document(self, collection_name, query, new_values):
+        collection = self.db[collection_name]
+        result = collection.find_one_and_update(query, {"$set": new_values}, return_document=ReturnDocument.AFTER)
+        return self._serialize(result)
+
+    def delete_documents(self, collection_name, query):
+        collection = self.db[collection_name]
+        result = collection.delete_many(query)
+        return result.deleted_count
+
+    def delete_document(self, collection_name, query):
+        collection = self.db[collection_name]
+        result = collection.find_one_and_delete(query)
+        return self._serialize(result)
+
+    def _serialize(self, document):
+        if document is None:
+            return None
+        if '_id' in document:
+            document['_id'] = str(document['_id'])
+        return document
+    
+    
+    
+class Service:
+    REGISTER_SUCCESS = 200
+    REGISTER_USER_EXISTS = 400
+    REGISTER_FAILURE = 500
+    SUCCESS=200
+
+    LOGIN_SUCCESS = 200
+    LOGIN_FAILURE = 401
+    def __init__(self):
+        self.db = MongoDB('taskmanager')
+    
+    def login_user(self, username, password):
+        return self.db.read_document('users', {'user_name': username, 'password': password})
+    
+    def register_user(self, data):
+        existing_user = self.db.read_document('users', {'user_name': data['user_name']})
+        if existing_user:
+            return {"status":self.REGISTER_USER_EXISTS, 'message':'User Already Registered'}
+        else:
+            data['onboading_flow_completed'] = False
+            data['dept'] = None
+            data['role'] = None
+            data['reports_to']  = None
+            user_id = self.db.create_document('users', data)
+            if user_id:
+                return {"status":self.REGISTER_SUCCESS, "message":'User Successfully Registered'}
+            else:
+                return {"status":self.REGISTER_FAILURE, "message":'User Registration Failed'}
+    
+    
+    def get_user(self, user_id):
+        data = self.db.read_document('users',{'_id':ObjectId(user_id)})
+        return {'status':self.SUCCESS, 'data':data}
+    
+    def set_onboading(self, user_id, data):
+        user = self.get_user(user_id)['data']
+        query={'_id':ObjectId(user.get('_id'))}
+        if user:
+            updated_data = {
+                'dept':data.get('dept'),
+                'role':data.get('role'),
+                'onboading_flow_completed':True,
+                'reports_to':data.get('reports_to')
+            }
+            res = self.db.update_document('users', query, updated_data)
+            
+            if res:
+                return {'status':200, 'data':res}
+            else:
+                return {'status': 500, 'data':'Error failed to update'}
+            
+    def get_onboarding(self):
+        depts = self.db.read_documents('dept')
+        return {'status':200, 'data': depts}
+    
+    def create_task(self, data):
+        res = self.db.create_document('tasks', data)
+        return res
+    
+    def update_task_status(self, task_id, status):
+        res = self.db.update_document('tasks', {'_id':ObjectId(task_id)}, {'status':status})
+        return res
+    
+    def update_task_data(self, task_id, new_data):
+        res = self.db.update_document('tasks', {'_id':ObjectId(task_id)}, new_data)
+        return res
+            
+    def get_tasks(self, is_admin, user_id): 
+        res = self.db.read_documents('tasks', {'created_by':user_id} if is_admin else {'assigned_to':user_id})
+        return res
---
+++ main.py
@@ -0,0 +1,160 @@
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, decode_token
+from dotenv import load_dotenv
+import datetime
+from db_service import Service
+from statusCodes import status_codes
+from flask_mail import Mail
+from email_service import gen_msg
+
+load_dotenv('.env')
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'str0m141v@gmail.com'
+app.config['MAIL_PASSWORD'] = os.getenv('GMAIL_APP_PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
+mail = Mail(app)
+CORS(app)
+jwt = JWTManager(app)
+db_service = Service()
+
+
+@app.before_request
+def check_token_expiry():
+    if request.path == '/api/login':
+        return None 
+    
+    if request.path == '/api/register':
+        return None 
+
+    auth_header = request.headers.get('Authorization')
+    if auth_header and auth_header.startswith('Bearer'):
+        token = auth_header.split()[1]
+        decoded_token = decode_token(token)
+        exp_time = datetime.datetime.fromtimestamp(decoded_token['exp']) 
+        current_time = datetime.datetime.now()  
+
+        if current_time > exp_time:
+            return jsonify({"error": "Expired token"}), 401
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    user_data = db_service.login_user(username, password)
+    if user_data:
+        expires = datetime.timedelta(days=1)
+        access_token = create_access_token(identity=user_data.get('_id'), expires_delta=expires)
+        user_data.pop('password')
+        user_data['access_token'] = access_token
+        return jsonify(user_data), 200
+    return jsonify({"error": "Invalid username or password"}), 401
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    reg_data = {
+        'user_name': data.get('username'),
+        'password': data.get('password'),
+        'created_at': datetime.datetime.now(),
+        'display_name': data.get('display_name'),
+        'email':data.get('email'),
+    }
+    res = db_service.register_user(reg_data)
+    if status_codes.get(res['status']):
+        return jsonify(res), res['status']
+    else:
+        return jsonify(res), res['status']
+
+
+@app.route('/api/get-user', methods=['GET'])
+@jwt_required()
+def get_user():
+    uid = get_jwt_identity()
+    res = db_service.get_user(uid)
+    res['data'].pop('password')
+    return jsonify(res), 200
+
+
+@app.route('/api/onboarding', methods=["POST", 'GET'])
+@jwt_required()
+def onboarding():
+    uid = get_jwt_identity()
+    if request.method == 'POST':
+        data = request.get_json()
+        res = db_service.set_onboading(uid, data)
+        if status_codes.get(res['status']):
+            return jsonify(res), res['status']
+        else:
+            return jsonify(res), res['status']
+    else:
+        res = db_service.get_onboarding()
+        return jsonify(res)
+
+
+@app.route('/api/tasks/add-task', methods=["POST"])
+@jwt_required()
+def add_task():
+    data = request.get_json()
+    res = db_service.create_task(data)
+    if res:
+        return jsonify(res), 200
+    else:
+        return jsonify(res), 401
+    
+@app.route("/api/tasks/update-status", methods=["POST"])
+@jwt_required()
+def update_status():
+    data = request.get_json()
+    res = db_service.update_task_status(data.get('id'), data.get('status'))
+    if res:
+        return jsonify(res), 200
+    else:
+        return jsonify(res), 401
+
+@app.route("/api/tasks/update-task", methods=["POST"])
+@jwt_required()
+def update_task():
+    data = request.get_json()
+    res = db_service.update_task_data(data.get('id'), data)
+    if res:
+        return jsonify(res), 200
+    else:
+        return jsonify(res), 401
+    
+@app.route('/api/tasks/list', methods=['POST'])
+@jwt_required()
+def list_tasks():
+    req = request.get_json()
+    if created_by := req.get("created_by"):
+        res = db_service.get_tasks(True, created_by)
+        return jsonify(res), 200
+    else:
+        res = db_service.get_tasks(False, req.get('assigned_to'))
+        return res, 200
+
+@app.route("/api/send-mail", methods=["POST"])
+@jwt_required()
+def index():
+    req = request.get_json()
+    status = req.get('status')
+    data = req.get('data')
+    msg = gen_msg(status=status, data=data)
+    try:
+        mail.send(msg)
+        return {"status":200, }
+    except Exception as e:
+        return {"status":401, "message":e}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
