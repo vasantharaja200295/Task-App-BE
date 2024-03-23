9fd6fe7fb1037e5811e014698e88cafd091f57b5
Vasantharaja <vasantharaja200295@gmail.com>
2024-03-23 11:50:37 +0530

    v0.0.001

--- email_service.py
+++ email_service.py
@@ -5,7 +5,7 @@ from flask import render_template
 class email_service:
     def gen_msg(self, status, data):
         reciever = data.get('assigned_to_email')
-        subject = f"Task Created: {data.get("task_name")}"
+        subject = f"Task Created: {data.get('task_name')}"
         content = render_template(f"{status}.html", data=data)
         
         msg = Message(recipients=[reciever], subject=subject)
